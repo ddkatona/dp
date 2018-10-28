efb7eac7418203cd522859ca128186540fe7a4b6
ddkatona <ddkatona@gmail.com>
2018-10-28 19:00:03 +0100

    khf2 working inefficient

--- khf2.erl
+++ khf2.erl
@@ -1,11 +1,16 @@
 -module(khf2).
 -author('ddkatona@gmail.com').
--vsn('2018-10-15').
--export([tipp_kod/2, lista_zsak/1, rek/3, get_lists/2]).
+-vsn('2018-10-28').
+-export([tipp_kod/2]).
 %-compile(export_all).
 
 tipp_kod(Max, Tipp_S) -> 
-	[H|T] = element(1, Tipp_S).
+	[H|T] = element(1, Tipp_S),
+	Plus_one = lists:seq(1,Max,1) ++ [H|T],
+	LZS = lista_zsak(Plus_one),
+	F = lists:map(fun(X) -> X - 1 end, LZS),
+	DUP = lists:duplicate(erlang:length(F),erlang:length([H|T])),
+	rek(DUP, F, element(2, Tipp_S)).
 	
 
 lista_zsak([]) -> [];
@@ -16,8 +21,6 @@ rekurzio([],[],K) ->
 		true -> [[]];
 		false -> []
 	end;
-%rekurzio([L],[0],0) -> [[L]];
-%rekurzio([H1|[]],[H2g|[]],H3) -> [];
 rekurzio(H, L, K) ->
 	case hd(H) < 0 of
 		true -> [];
@@ -32,18 +35,40 @@ rekurzio(H, L, K) ->
 			end
 	end.	
 
-rek(H, L, K) ->
-	get_lists(lists:sum(L), rekurzio(H,L,K)).
+rek(H, _, K) when K > hd(H) -> [];
+rek(H, L, K) when K == hd(H) -> perm(L,1,1);
+rek(H, L, K) when K < hd(H) ->
+	Zsak = get_lists(lists:sum(L), rekurzio(H,L,K)),
+	append_all(Zsak).
+
+append_all([]) -> [];
+append_all(O) ->
+	perm(hd(O),1,1) ++ append_all(tl(O)).
 
 get_lists(Size, L) -> lists:filter(fun(X) -> lists:sum(X) =:= Size end, L).
 
-missing_elements(0, L) -> [];
-missing_elements(Max, L) -> 
-	case lists:member(Max, L) of 
-		false -> R = [Max|missing_elements(Max-1, L)];
-		true -> R = missing_elements(Max-1, L)
-	end,
-	R.	
+% perm([2, 1, 2, 4], 1, 0).
+perm([], _, _) -> [[]];
+perm([1], N, _) -> [[N]];
+perm(L, _, I) when erlang:length(L) < I -> [];
+perm(L, N, I) ->
+	case hd(L) == 0 of
+		true -> 
+			perm(tl(L), N+1, 1);
+		false ->
+			case lists:nth(I,L) == 0 of
+				true -> 
+					L1 = [],
+					L2 = perm(L, N, I+1),
+					L1 ++ L2;
+				false ->
+					{L1a,L1b} = lists:split(I-1,L),
+					L1b2 = [hd(L1b)-1|tl(L1b)],		
+					L1 = [[N+I-1|T] || T <- perm(L1a ++ L1b2, N, 1)],
+					L2 = perm(L, N, I+1),
+					L1 ++ L2
+			end	
+	end.
 
 count_element([],_) -> 0;
 count_element(L, E) -> 
